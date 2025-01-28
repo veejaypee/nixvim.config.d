9323103c7cd0092745fd6eadb030fda187e07c85
v-j-p <v.poigner@gmail.com>
2025-01-28 16:45:51 +0100

    catppuccin colorscheme

--- config/default.nix
+++ config/default.nix
@@ -7,8 +7,14 @@
     ./keymaps.nix
     ./plugins.nix
   ];
-    opts = {
-      number = true;
-      relativenumber = true;
-};
+  opts = {
+    number = true;
+    relativenumber = true;
+  };
+  colorschemes.catppuccin = {
+    enable = true;
+    settings = {
+      flavour = "mocha";
+    };
+  };
 }

28cbe985a974939a1fed61b1ce7bd10d6dfe8abc
v-j-p <v.poigner@gmail.com>
2025-03-08 15:31:01 +0100

    typo2

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -3,68 +3,68 @@
   config,
   ...
 }: {
-    globals.mapleader = " ";
+  globals.mapleader = " ";
 
-    keymaps = [
-      {
-        action = "<cmd>oil<cr>";
-        key = "-";
-        mode = ["n"];
-      }
-      {
-        action = "<cmd>Telescope<cr>";
-        key = "<leader>f<leader>";
-        mode = ["n"];
-      };
-    ];
+  keymaps = [
+    {
+      action = "<cmd>oil<cr>";
+      key = "-";
+      mode = ["n"];
+    }
+    {
+      action = "<cmd>Telescope<cr>";
+      key = "<leader>f<leader>";
+      mode = ["n"];
+    }
+  ];
 
-    plugins = {
-      lsp.keymaps = {
-        diagnostic = {
-          "[d" = {
-            action = "goto_next";
-            desc = "Goto next diagnostic";
-          };
-          "]d" = {
-            action = "goto_prev";
-            desc = "Goto previous diagnostic";
-          };
-          "gl" = {
-            action = "open_float";
-            desc = "Open float";
-          };
+  plugins = {
+    lsp.keymaps = {
+      diagnostic = {
+        "[d" = {
+          action = "goto_next";
+          desc = "Goto next diagnostic";
         };
+        "]d" = {
+          action = "goto_prev";
+          desc = "Goto previous diagnostic";
+        };
+        "gl" = {
+          action = "open_float";
+          desc = "Open float";
+        };
+      };
 
-        lspBuf = {
-          K = {
-            action = "hover";
-            desc = "Hover";
-          };
-          gr = {
-            action = "references";
-            desc = "Goto references";
-          };
-          gd = {
-            action = "definition";
-            desc = "Goto definition";
-          };
-          gi = {
-            action = "implementation";
-            desc = "Goto implementation";
-          };
-          gt = {
-            action = "type_definition";
-            desc = "Type definition";
-          };
-          gs = {
-            action = "signature_help";
-            desc = "Signature help";
-          };
-          "<leader>carn" = {
-            action = "rename";
-            desc = "Rename variable";
-          };
+      lspBuf = {
+        K = {
+          action = "hover";
+          desc = "Hover";
+        };
+        gr = {
+          action = "references";
+          desc = "Goto references";
+        };
+        gd = {
+          action = "definition";
+          desc = "Goto definition";
+        };
+        gi = {
+          action = "implementation";
+          desc = "Goto implementation";
+        };
+        gt = {
+          action = "type_definition";
+          desc = "Type definition";
+        };
+        gs = {
+          action = "signature_help";
+          desc = "Signature help";
+        };
+        "<leader>carn" = {
+          action = "rename";
+          desc = "Rename variable";
         };
       };
     };
-  }
+  };
+}

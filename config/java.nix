645d6a0fba18a097f05c69cb8ca4cb30ce2671e8
Veit Poigner <v.poigner@gmail.com>
2025-11-09 21:06:12 +0100

    chore: adds the window cahngeing to control + movement

--- config/java.nix
+++ config/java.nix
@@ -11,6 +11,4 @@
       };
     };
   };
-  keymapsOnEvents = {
-  };
 }
--- config/keymaps.nix
+++ config/keymaps.nix
@@ -7,6 +7,30 @@
 
   keymaps = [
     {
+      action = "<C-w><C-h>";
+      key = "<C-h>";
+      mode = ["n"];
+      options.desc = "Move focus to the left window";
+    }
+    {
+      action = "<C-w><C-l>";
+      key = "<C-l>";
+      mode = ["n"];
+      options.desc = "Move focus to the right window";
+    }
+    {
+      action = "<C-w><C-j>";
+      key = "<C-j>";
+      mode = ["n"];
+      options.desc = "Move focus to the lower window";
+    }
+    {
+      action = "<C-w><C-k>";
+      key = "<C-k>";
+      mode = ["n"];
+      options.desc = "Move focus to the upper window";
+    }
+    {
       action = "<cmd>Oil<cr>";
       key = "-";
       mode = ["n"];
@@ -19,6 +43,9 @@
       options.desc = "[B]uffer [D]elete [O]thers";
     }
   ];
+
+  keymapsOnEvents = {
+  };
 
   plugins = {
     telescope.keymaps = {

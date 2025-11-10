d301fa355a0c9bacd184e782481e24310200fbb7
Veit Poigner <v.poigner@gmail.com>
2025-11-10 14:01:08 +0100

    feat: adds diagnostic info and styling

--- config/default.nix
+++ config/default.nix
@@ -58,4 +58,41 @@
       flavour = "mocha";
     };
   };
+  diagnostic = {
+    settings = {
+      severity_sort = true;
+      float = {
+        border = "rounded";
+        source = "if_many";
+      };
+      underline = {
+        severity.__raw = ''vim.diagnostic.severity.ERROR'';
+      };
+      signs.__raw = ''
+        vim.g.have_nerd_font and {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
+          },
+        } or {}
+      '';
+      virtual_text = {
+        source = "if_many";
+        spacing = 2;
+        format.__raw = ''
+          function(diagnostic)
+            local diagnostic_message = {
+              [vim.diagnostic.severity.ERROR] = diagnostic.message,
+              [vim.diagnostic.severity.WARN] = diagnostic.message,
+              [vim.diagnostic.severity.INFO] = diagnostic.message,
+              [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+          end
+        '';
+      };
+    };
+  };
 }
